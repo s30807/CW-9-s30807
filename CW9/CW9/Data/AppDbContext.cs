ad04966e0855f0c1bb4da87ad89c36c5d481aa03
s30807 <s30807@pjwstk.edu.pl>
2025-05-28 18:01:11 +0200

    Not finished. Set up WebApp

---
+++ CW9/CW9/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CW9.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CW9.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Doctor> Students { get; set; }
+    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
